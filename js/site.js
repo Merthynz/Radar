b061f42680fc3f2b04838fab287a2a549ac6c4e2
Merthynz <rerive@gmail.com>
2022-09-26 00:04:35 -0400

    Fill up App page  Changes to be committed: 	new file:   .vscode/launch.json 	modified:   app.html 	modified:   code.html 	modified:   css/site.css 	new file:   img/radar_logo.png 	deleted:    img/ziento_logo.png 	modified:   index.html 	modified:   js/site.js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,93 @@
-// Call Hello World
-function helloWorld() {
-    alert("Hello World");
+// Get our user input
+function getValue() {
+    
+    // Make sure the alert is invisible
+    document.getElementById("alert").classList.add("invisible");
+    
+    // Get user string from the page
+    let userString = document.getElementById("userString").value;
+
+    // Check for a palindrome
+    let returnObj = checkForPalindrome(userString);
+
+    // Display our message to the screen
+    displayMessage(returnObj);
 }
+
+// Check if string is a palindrome
+function checkForPalindrome(userString) {
+
+
+    // taco cat
+    userString = userString.toLowerCase();
+
+    // Remove spaces and special characters
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex, "");
+
+    let revString = [];
+    let returnObj = {};
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    if (revString == userString) {
+        returnObj.msg = "Well done! You entered a palindrome!"
+    } else {
+        returnObj.msg = "Sorry! You did not enter a palindrome!"
+    } 
+
+    returnObj.reversed = revString;
+
+    return returnObj;
+    
+}
+
+// Display a message to the string.
+function displayMessage(returnObj) {
+
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+    document.getElementById("alert").classList.remove("invisible");
+}
+
+// *****************************************************************
+/* Solution for reversible
+
+// Get the string from the page
+// Controller function
+function getValue(){
+    
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+// Revese the string
+// Logic function 
+function reverseString(userString){
+
+    let revString = [];
+    
+    // Reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+    return revString;
+}
+
+// Display our reversed string to the user
+// View function
+function displayString(revstring){
+
+    // Write to the page
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${revstring}`;
+    document.getElementById("alert").classList.remove("invisible");
+}
+
+*/
